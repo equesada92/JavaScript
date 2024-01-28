cf2509424add5bf3eacf44ba3453c3eee3b14f68
equesada92 <equesadaz92@gmail.com>
2024-01-28 11:43:16 -0600

    Ejercicio 3

---
+++ Ejercicio 3/Ej3.js
@@ -0,0 +1,12 @@
+const data= ["hola",2,5,"adios"]
+if (data[1]>data[2]){
+    console.log("El numero mayor es: "+data[1])}
+else{
+    console.log("El numero mayor es: "+data[2])
+    }
+
+//Operaciones aritméticas básicas con los numeros:
+console.log("Suma: "+(data[2]+data[1]));
+console.log("Resta: "+(data[2]-data[1]));
+console.log("Multiplicación: "+(data[2]*data[1]));
+console.log("División: "+(data[2]/data[1]));
