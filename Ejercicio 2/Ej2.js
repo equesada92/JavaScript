190ed159c7b631197f4f65ae1156823798897687
equesada92 <equesadaz92@gmail.com>
2024-01-28 11:23:48 -0600

    ejercicio2

---
+++ Ejercicio 2/Ej2.js
@@ -0,0 +1,10 @@
+const cumple={
+    dia:24,
+    mes:'Noviembre',
+    año:1992,
+};
+console.log('Mi cumpleaños es el '+cumple.dia+' de '+cumple.mes+' de '+cumple.año)
+
+//Meses del año
+const years=['Enero','Febrero','Marzon','Abril','Mayo','Junio','Julio','Agosto','Setiembre','Octubre','Noviembre','Diciembre']
+console.log(years[11])
