298d274ca519dc486d14d7c59260477478942ad2
equesada92 <equesadaz92@gmail.com>
2024-01-30 19:36:39 -0600

    Ejercicio 12

---
+++ Ejercicio 12/Ej12.js
@@ -0,0 +1,8 @@
+const topics = ["JavaScript","Variables","funciones","condicionales","bucles"];
+const topicsUpper=topics.map(function(topics){
+    return topics.toUpperCase();
+})
+const resultado=topicsUpper.reverse();
+console.log (resultado)
+
+    
